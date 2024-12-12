0a3a5e82785cecee77d667073ef0a0c6b10f0222
Hamza Hafdaoui <hamza.hafdaoui@etu.uae.ac.ma>
2024-12-12 20:41:20 +0100

    all

--- backend/src/routes/images.js
+++ backend/src/routes/images.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { uploadImages, getImagesByCategory } = require('../controllers/imageController'); // Ensure these controllers exist
+const { uploadImages, getImagesByCategory} = require('../controllers/imageController'); // Ensure these controllers exist
 const upload = require('../middleware/uploadMiddleware'); // Ensure this is configured properly
 
 // Route to handle image uploads
@@ -8,5 +8,6 @@ router.post('/upload', upload.array('images', 10), uploadImages); // 'images' is
 
 // Route to fetch images by category
 router.get('/:category', getImagesByCategory);
+
 
 module.exports = router;
